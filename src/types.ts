59791b6e60f3c5d005af5f5d597f52f506e491a3
dakewamama <davidasiadiachi@gmail.com>
2026-06-16 09:36:15 +0200

    types for program event structs

---
+++ src/types.ts
@@ -0,0 +1,69 @@
+export type Address = string;
+
+export interface SubscriptionCreatedEvent {
+    type: "SubscriptionCreated";
+    plan: Address;
+    subscriber: Address;
+    mint: Address;
+    createdTS: bigint;
+}
+
+export interface SubscriptionCancelledEvent {
+    type: "SubscriptionCancelled";
+    plan: Address;
+    subscriber: Address;
+    expiresAtTs: bigint;
+}
+
+export interface SubscriptionTransferEvent {
+    type: "SubscriptionTransfer";
+    subscription: Address;
+    plan: Address;
+    delegator: Address;
+    mint: Address;
+    amount: bigint;
+    periodStartTs: bigint;
+    periodEndTs: bigint;
+    amountPulledInPeriod: bigint;
+    receiver: Address;
+
+}
+
+export interface FixedTransferEvent {
+    type: "FixedTransfer";
+    delegation: Address;
+    delegator: Address;
+    delegatee: Address;
+    mint: Address;
+    amount: bigint;
+    remainingAmount: bigint;
+    receiver: Address;
+}
+
+export interface RecurringTransferEvent {
+    type: "RecurringTransfer";
+    delegation: Address;
+    delegator: Address;
+    delegatee: Address;
+    mint: Address;
+    amount: bigint;
+    periodStartTs: bigint;
+    periodEndTs: bigint;
+    amountPulledInPeriod: bigint;
+    receiver: Address;
+}
+
+export interface SubscriptionResumedEvent {
+    type: "SubscriptionResumed";
+    plan: Address;
+    subscriber: Address;
+    resumedTS: bigint;
+}
+
+export type CatalystEvent =
+    | SubscriptionCreatedEvent
+    | SubscriptionCancelledEvent
+    | SubscriptionTransferEvent
+    | FixedTransferEvent
+    | RecurringTransferEvent
+    | SubscriptionResumedEvent;
