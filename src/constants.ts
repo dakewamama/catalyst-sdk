ceb53f1cfcab58a8a68b3d02416c6568899321de
dakewamama <davidasiadiachi@gmail.com>
2026-06-16 08:18:30 +0200

    started initials with constants

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SUBSCRIPTION_PROGRAM_ID = "De1egAFMkMWZSN5rYXRj9CAdheBamobVNubTsi9avR44";
+
+// 8-byte discriminator for self-CPI events
+export const EVENT_IX_TAG = new Uint8Array([0xe4, 0x45, 0xa5, 0x2a, 0x0a, 0xa1, 0x56, 0xe1]);
